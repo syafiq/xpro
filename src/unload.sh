eb81304a12dfcaa76794ad6d1231b2a49f925dd5
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2020-07-20 18:19:06 +0200

    rm dir so that I dont have to use reuse-maps option

--- src/unload.sh
+++ src/unload.sh
@@ -1,1 +1,5 @@
 sudo ./xdp_loader --unload --dev lo --skb-mode
+sudo rm -R /sys/fs/bpf/lo/xdp_stats_map
+sudo rm -R /sys/fs/bpf/lo/ts1
+sudo rm -R /sys/fs/bpf/lo/ts2
+sudo rm -R /sys/fs/bpf/lo/counter_c
