81a15f68c6817b500b116077bca8a0bffa3cb74d
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2020-07-20 21:00:48 +0200

    change interface

--- src/load.sh
+++ src/load.sh
@@ -1,1 +1,1 @@
-sudo strace ./xdp_loader --dev lo --skb-mode
+sudo strace ./xdp_loader --dev enp0s10 --skb-mode
--- src/unload.sh
+++ src/unload.sh
@@ -1,5 +1,5 @@
-sudo ./xdp_loader --unload --dev lo --skb-mode
-sudo rm -R /sys/fs/bpf/lo/xdp_stats_map
-sudo rm -R /sys/fs/bpf/lo/ts1
-sudo rm -R /sys/fs/bpf/lo/ts2
-sudo rm -R /sys/fs/bpf/lo/counter_c
+sudo ./xdp_loader --unload --dev enp0s10 --skb-mode
+sudo rm -R /sys/fs/bpf/enp010/xdp_stats_map
+sudo rm -R /sys/fs/bpf/enp0s10/ts1
+sudo rm -R /sys/fs/bpf/enp0s10/ts2
+sudo rm -R /sys/fs/bpf/enp0s10/counter_c
