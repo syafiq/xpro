4dfdb2be85e2acee66c5ff42d6414cfaeee0835a
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2020-09-28 08:24:46 +0200

    everything now inside load.sh

--- src/load.sh
+++ src/load.sh
@@ -1,1 +1,10 @@
-sudo strace ./xdp_loader --dev ens3 --skb-mode
+# unload
+sudo ./xdp_loader --unload --dev ens3 --skb-mode
+sudo rm -R /sys/fs/bpf/ens3/xdp_stats_map
+sudo rm -R /sys/fs/bpf/ens3/mapall
+
+# load
+sudo ./xdp_loader --dev ens3 --skb-mode
+
+# listener uspace
+sudo ./xpro_uspace
