d489935a55d84ebb1a01440b5ad52e0ff8c38870
syafiq <syafiq@cas-23.eit.lth.se>
2020-10-08 10:33:58 +0200

    fix load unload

--- src/load.sh
+++ src/load.sh
@@ -1,10 +1,11 @@
 # unload
-sudo ./xdp_loader --unload --dev ens3 --skb-mode
-sudo rm -R /sys/fs/bpf/ens3/xdp_stats_map
-sudo rm -R /sys/fs/bpf/ens3/mapall
+sudo ./xdp_loader --unload --dev enp2s0 --skb-mode
+sudo rm -R /sys/fs/bpf/enp2s0/xdp_stats_map
+sudo rm -R /sys/fs/bpf/enp2s0/mapall
+sudo rm -R /sys/fs/bpf/enp2s0/tdiff
 
 # load
-sudo ./xdp_loader --dev ens3 --skb-mode
+sudo ./xdp_loader --dev enp2s0 --skb-mode
 
 # listener uspace
 # sudo ./xpro_uspace
--- src/unload.sh
+++ src/unload.sh
@@ -1,3 +1,4 @@
-sudo ./xdp_loader --unload --dev ens3 --skb-mode
-sudo rm -R /sys/fs/bpf/ens3/xdp_stats_map
-sudo rm -R /sys/fs/bpf/ens3/mapall
+sudo ./xdp_loader --unload --dev enp2s0 --skb-mode
+sudo rm -R /sys/fs/bpf/enp2s0/xdp_stats_map
+sudo rm -R /sys/fs/bpf/enp2s0/mapall
+sudo rm -R /sys/fs/bpf/enp2s0/tdiff
