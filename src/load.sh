93f4fc866001ccbf4966cd549d5ee67e7decd396
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2020-10-21 06:00:05 +0200

    no delete for tdiff

--- src/load.sh
+++ src/load.sh
@@ -2,7 +2,7 @@
 sudo ./xdp_loader --unload --dev ens10 --skb-mode
 sudo rm -R /sys/fs/bpf/ens10/xdp_stats_map
 sudo rm -R /sys/fs/bpf/ens10/mapall
-sudo rm -R /sys/fs/bpf/ens10/tdiff
+#sudo rm -R /sys/fs/bpf/ens10/tdiff
 sudo rm -R /sys/fs/bpf/ens10/stats
 
 # load
