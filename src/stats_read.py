4fe8d8d0fd571aa587681a62170c5a6cd98d6f14
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2020-12-16 02:21:12 +0100

    stats_read python version to make life easier

---
+++ src/stats_read.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+while (1):
+    drop_gen1 = os.popen("sudo bpftool map lookup name stats key 01 00 00 00").read()
+    pass_gen1 = os.popen("sudo bpftool map lookup name stats key 02 00 00 00").read()
+    drop_gen2 = os.popen("sudo bpftool map lookup name stats key 03 00 00 00").read()
+    pass_gen2 = os.popen("sudo bpftool map lookup name stats key 04 00 00 00").read()
+    
+    def print_res(inp):
+        inp = inp.split("\n")
+        if (inp[3] == "Not found"):
+            return -1
+        else:
+            val = inp[2]
+            val = val.split(":")
+            val = val[1].split(" ")
+            lval = len(val)-1
+            out = ""
+            while (val[lval] != ''):
+                if (val[lval] != "00"):
+                    out += val[lval]
+                lval -= 1
+            return int(out,16)
+    
+    dg1 = print_res(drop_gen1)
+    pg1 = print_res(pass_gen1)
+    dg2 = print_res(drop_gen2)
+    pg2 = print_res(pass_gen2)
+    
+    print("drop_gen1",dg1,"pass_gen1",pg1,"drop_gen2",dg2,"pass_gen2",pg2)
+    time.sleep(2)
